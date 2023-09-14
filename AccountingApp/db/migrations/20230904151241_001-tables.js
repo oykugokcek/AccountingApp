3890ec0f0b9c044f03e690714d413cf00e7a24cb
oykugokcek <oykgokcek@gmail.com>
2023-09-14 20:22:00 +0300

    seed file changed

--- AccountingApp/db/migrations/20230904151241_001-tables.js
+++ AccountingApp/db/migrations/20230904151241_001-tables.js
@@ -2,60 +2,83 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
 exports.up = function(knex) {
-  return Promise.all([
-    knex.schema.createTable("address", (table) => {
-      table.increments("id").primary();
-      table.string("neighborhood");
-      table.string("district").notNullable();
-      table.string("city").notNullable();
-      table.timestamps(true, true);
-    }),
-    knex.schema.createTable("factory", (table) => {
-      table.increments("id").primary();
-      table.string("name").notNullable().unique();
-      table.string("tax_number");
-      table.integer("people_id").unsigned();
-      table.foreign("people_id").references("id").inTable("people");
-      table.string("branch");
-      table.string("address");
-      table.timestamps(true, true);
-    }),
-    knex.schema.createTable("vehicle", (table) => {
-      table.increments("id").primary();
-      table.string("plate_number").notNullable().unique();
-      table.string("type");
-      table.integer("owner_id").unsigned();
-      table.foreign("owner_id").references("id").inTable("owner");
-      table.integer("driver_id").unsigned();
-      table.foreign("driver_id").references("id").inTable("driver");
-      table.timestamps(true, true);
-    }),
-    knex.schema.createTable("driver", (table) => {
-      table.increments("id").primary();
-      table.string("name").notNullable();
-      table.string("tc").unique();
-      table.string("phone_number").unique();
-      table.timestamps(true, true);
-    }),
-    knex.schema.createTable("owner", (table) => {
-      table.increments("id").primary();
-      table.string("name").notNullable();
-      table.string("tc").unique();
-      table.integer("address_id").unsigned();
-      table.foreign("address_id").references("id").inTable("address");
-      table.string("phone_number").unique();
-      table.string("iban");
-      table.timestamps(true, true);
-    }),
-    knex.schema.createTable("product", (table) => {
-      table.increments("id").primary();
-      table.string("name").notNullable().unique();
-      table.float("price");
-      table.timestamps(true, true);
-    })
-  ]);
-};
+  // return Promise.all([
+  //   knex.schema.dropTableIfExists("factory"),
+  //   knex.schema.dropTableIfExists("address"),
+  //   knex.schema.dropTableIfExists("factory_branch"),
+  //   knex.schema.dropTableIfExists("driver"),
+  //   knex.schema.dropTableIfExists("vehicle"),
+  //   knex.schema.dropTableIfExists("transport"),
+  //   knex.schema.dropTableIfExists("product"),
+  //   knex.schema.dropTableIfExists("waybill"),
+  // ])
+  // .then(() => {
+    return Promise.all([
+      knex.schema.createTable("factory", (table) => {
+        table.increments("id").primary();
+        table.string("name");
+        table.string("tax_number").unique();
+       
+      }),
+      knex.schema.createTable("address", (table) => {
+        table.increments("id").primary();
+        table.string("city").notNullable();
+        table.string("district").notNullable().unique();
+        table.string("neighborhood");
+        table.string("street").unique();
+        table.integer("post_code").unique();
+       
+      }),
+      knex.schema.createTable("factory_branch", (table) => {
+        table.increments("id").primary();
+        table.integer("factory_id").unsigned();
+        table.foreign("factory_id").references("id").inTable("factory");
+        table.integer("address_id").unsigned();
+        table.foreign("address_id").references("id").inTable("address");
+        table.timestamps(true, true);
+      }),
+      knex.schema.createTable("driver", (table) => {
+        table.increments("id").primary();
+        table.string("first_name").notNullable();
+        table.string("last_name").notNullable();
+        table.string("tc_number").notNullable();
+      }),
+      knex.schema.createTable("vehicle", (table) => {
+        table.increments("id").primary();
+        table.string("plate_number").notNullable().unique();
+        table.string("capacity").notNullable();
+      }),
+      knex.schema.createTable("transport", (table) => {
+        table.increments("id").primary();
+        table.integer("driver_id").unsigned();
+        table.foreign("driver_id").references("id").inTable("driver");
+        table.integer("vehicle_id").unsigned();
+        table.foreign("vehicle_id").references("id").inTable("vehicle");
+        table.timestamps(true, true);
+      }),
+      knex.schema.createTable("product", (table)=> {
+        table.increments("id").primary();
+        table.string("name").notNullable();
+        table.float("price").notNullable();
+        table.timestamps(true, true);
+      }),
+      knex.schema.createTable("waybill", (table) => {
+        table.increments("id").primary();
+        table.integer("address_id").unsigned();
+        table.foreign("address_id").references("id").inTable("address");
+        table.integer("factory_branch_id").unsigned();
+        table.foreign("factory_branch_id").references("id").inTable("factory_branch");
+        table.integer("transport_id").unsigned();
+        table.foreign("transport_id").references("id").inTable("transport");
+        table.integer("product_id").unsigned();
+        table.foreign("product_id").references("id").inTable("product");
+        table.timestamps(true, true);
+      })
+    ]);}
+//   }) 
+// };
 
 /**
  * @param { import("knex").Knex } knex
@@ -63,11 +86,14 @@ exports.up = function(knex) {
  */
 exports.down = function(knex) {
   return Promise.all([
+    knex.schema.dropTableIfExists("waybill"),
     knex.schema.dropTableIfExists("product"),
-    knex.schema.dropTableIfExists("owner"),
-    knex.schema.dropTableIfExists("driver"),
+    knex.schema.dropTableIfExists("transport"),
     knex.schema.dropTableIfExists("vehicle"),
-    knex.schema.dropTableIfExists("factory"),
+    knex.schema.dropTableIfExists("driver"),
+    knex.schema.dropTableIfExists("factory_branch"),
     knex.schema.dropTableIfExists("address"),
-  ]);
+    knex.schema.dropTableIfExists("factory")
+  ])
+  
 };
--- AccountingApp/db/seeds/initial_data.js
+++ AccountingApp/db/seeds/initial_data.js
@@ -6,13 +6,15 @@ exports.seed = async function(knex) {
 
    await knex.raw('SET FOREIGN_KEY_CHECKS=0');
 
+  await knex("waybill").truncate();
   await knex("product").truncate();
-  await knex("vehicle").truncate();
-  await knex("owner").truncate();
-  await knex("driver").truncate();  
-  await knex("people").truncate();
-  await knex("factory").truncate();
+  await knex("transport").truncate();
+  await knex("vehicle").truncate();  
+  await knex("driver").truncate();
+  await knex("factory_branch").truncate();
   await knex("address").truncate();
+  await knex("factory").truncate();
+  await knex("people").truncate();
 
 
    await knex.raw('SET FOREIGN_KEY_CHECKS=1');
@@ -37,93 +39,192 @@ exports.seed = async function(knex) {
 
   await knex("factory").insert([ 
     {
-      name: "Fabrika 1",
-      tax_number: "12345",
-      people_id: 1,
-      branch: "Şube 1",
-      address: "Adres 1"
+      name: "Tamek",
+      tax_number: "8170582135",
     },
     {
-      name: "Fabrika 2",
-      tax_number: "67890",
-      people_id: 2,
-      branch: "Şube 2",
-      address: "Adres 2"
-    }
+      name: "Döhler",
+      tax_number: "5760970424",
+    },
+    {
+      name: "Etap",
+      tax_number: "0691386511",
+    },
+    
   ]);
 
   await knex("address").insert([ 
     {
-      neighborhood: "Mahalle 1",
-      district: "İlçe 1",
-      city: "A"  // Örnek bir şehir ID'si
+      city: "Balıkesir",
+      district: "Merkez",
+      neighborhood: "Ovaköy",
+      street: null,
+      post_code:10185
     },
     {
-      neighborhood: "Mahalle 2",
-      district: "İlçe 2",
-      city: "B"  // Örnek bir şehir ID'si
+      city: "Bursa",
+      district: "Karacabey",
+      neighborhood: "Tavşanlı",
+      street: null,
+      post_code:16700
     },
-  ]);
+    {
+      city: "Denizli",
+      district: "Çal",
+      neighborhood: "Akçaköy",
+      street: null,
+      post_code:20710
+    },
+    {
+      city: "Denizli",
+      district: "Çivril",
+      neighborhood: "Akçaköy",
+      street: null,
+      post_code:20600
+    },
+    {
+      city: "Isparta",
+      district: "Gönen",
+      neighborhood: "Süleyman Demirel Or. Sanayi Bölgesi",
+      street: null,
+      post_code:32090
+    }, 
+    {
+      city: "İzmir",
+      district: "Kemalpaşa",
+      neighborhood: "Ören",
+      street: null
+    }, 
+    {
+      city: "Manisa",
+      district: "Turgutlu",
+      neighborhood: "Ören",
+      street: null
+    }, 
 
-  await knex("owner").insert([ 
+  ]);
+  
+
+
+
+  await knex("factory_branch").insert([ 
     {
-      name: "Sahibi 1",
-      tc: "34567890123",
-      address_id: 1,
-      phone_number: "3333",
-      iban: "IBAN123"
+      factory_id: 1,
+      address_id: 2
     },
     {
-      name: "Sahibi 2",
-      tc: "45678901234",
-      address_id: 2,
-      phone_number: "4444",
-      iban: "IBAN456"
-    }
+      factory_id: 2,
+      address_id: 1
+    },
+    {
+      factory_id: 2,
+      address_id: 3
+    },
+    {
+      factory_id: 3,
+      address_id: 4
+    },
+    {
+      factory_id: 3,
+      address_id: 5
+    },
+   
   ]);
 
 
   await knex("driver").insert([ 
     {
-      name: "Şoför 1",
-      tc: "12345678901",
-      phone_number: "1111"
+      first_name: "Süleyman",
+      last_name: "Çakır",
+      tc_number: 34576193446
     },
     {
-      name: "Şoför 2",
-      tc: "23456789012",
-      phone_number: "2222"
-    }
+      first_name: "Süleyman",
+      last_name: "Aydın",
+      tc_number: 28516339254
+    },
+    {
+      first_name: "Ergün",
+      last_name: "Filiz",
+      tc_number: 34852184276
+    },
+    {
+      first_name: "Ümit",
+      last_name: "Atasever",
+      tc_number: 62917225914
+    },
+    {
+      first_name: "Ceylan",
+      last_name: "Polat",
+      tc_number: 25621435772
+    },
+    
   ]);
-
-
   await knex("vehicle").insert([ 
     {
-      plate_number: "ABC123",
-      type: "Araç Tipi 1",
-      owner_id: 1,
-      driver_id: 1
+      plate_number: "17 AEN 396",
+      capacity:"large"
     },
     {
-      plate_number: "XYZ456",
-      type: "Araç Tipi 2",
-      owner_id: 2,
-      driver_id: 2
-    }
+      plate_number: "16 BBT 595",
+      capacity:"large"
+    },
+    {
+      plate_number: "16 FR 481",
+      capacity:"medium"
+    },
+    {
+      plate_number: "16 BRS 37",
+      capacity:"small"
+    },
+   
   ]);
 
 
- 
+  await knex("transport").insert([ 
+    {
+      driver_id:1,
+      vehicle_id:1
+    },
+    {
+      driver_id:2,
+      vehicle_id:2
+    },
+    {
+      driver_id:4,
+      vehicle_id:3
+    },
+    {
+      driver_id:5,
+      vehicle_id:4
+    },
+    
+  ]);
 
   await knex("product").insert([ 
     {
-      name: "Ürün 1",
-      price: 100.0
+      name: "Şeftali",
+      price: 6.20
     },
     {
-      name: "Ürün 2",
-      price: 200.0
+      name: "Elma",
+      price: 2.30
     }
+  ]);
+  await knex("waybill").insert([ 
+    {
+      address_id:6,
+      factory_branch_id:2,
+      transport_id:1,
+      product_id:1
+    },
+    {
+      address_id:7,
+      factory_branch_id:1,
+      transport_id:3,
+      product_id:2
+    },
+   
   ]);
 
 
